fbdece56d1de7460f8c07da19fd9a5a5b6b702ea
Niek Janssen <niek.janssen01@gmail.com>
2018-05-18 21:13:16 +0200

    niekBot

--- bots/niekBot.py
+++ bots/niekBot.py
@@ -8,13 +8,12 @@ from bots.BotLib import BotLib, Direction
 class NiekBot(BotLib):
     def __init__(self):
         super().__init__()
-        self.placementIndex = 0
         # placementIndex is used to choose an island and shot location. You may remove it if you do not use it.
 
     # random, not along borders and euclidean distance of at least 4
     def choose_island_location(self):
-        x = randint(1, 8)
-        y = randint(1, 8)
+        x = randint(1, 9)
+        y = randint(1, 9)
         for ix in range(-3, 4):
             for iy in range(-3 + abs(ix), 4 - abs(ix)):
                 if not self.checkFree((x + ix, y + iy), self.enemyBoard, True):
@@ -40,9 +39,24 @@ class NiekBot(BotLib):
 
     # Chance field
     def choose_shot_location(self):
-        # This is a dummy method, you should write a better one.
-        self.placementIndex += 1
-        return (int(self.placementIndex / 10), self.placementIndex % 10)
+        x = randint(0, 9)
+        y = randint(0, 9)
+
+        # scores = [[sum([min(5, self.checkRangeFree((x, y), d)) for d in range(0, 4)])
+        #           for y in range(0, 10)]
+        #          for x in range(0, 10)]
+        #
+        # max=0
+        # x=0
+        # y=0
+        # for x in range(0,10):
+        #     for y in range(0,10):
+        #         if max < scores[x][y]:
+        #             max = scores[x][y]:
+        #
+        # print(scores, file=sys.stderr)
+
+        return x, y
 
     def choose_ship_size(self):
         return super().choose_ship_size()
